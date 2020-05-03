7f86406082ea81ca12bf078600a5b1593b1f98bb
Yvette <290319704@qq.com>
2020-05-03 00:01:10 -0700

    add js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+
+window.scroll({
+  top: 0, 
+  left: 0, 
+  behavior: 'smooth'
+});
+
+
+
+/*
 $("button").click(function() {
     $('html,body').animate({
         scrollTop: $(".who-container").offset().top},
         'slow');
-});
+});*/
